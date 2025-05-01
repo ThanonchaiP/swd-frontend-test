a982caca49c5af599c685fb7f8b5156ba1c3eeae
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-05-01 21:22:56 +0700

    Add ArrowCard component and styles; implement layout in page

--- src/app/(apps)/layout-and-style/page.tsx
+++ src/app/(apps)/layout-and-style/page.tsx
@@ -3,6 +3,10 @@
 import { Col, Flex, Row, Typography } from "antd";
 import { useTranslation } from "react-i18next";
 
+import { ArrowCard } from "@/components/arrow-card";
+
+import styles from "./page.module.scss";
+
 export default function LayoutAndStyle() {
   const { t } = useTranslation();
 
@@ -10,13 +14,23 @@ export default function LayoutAndStyle() {
     <Flex vertical gap={16}>
       <Typography.Title level={1}>{t("layoutAndStyle")}</Typography.Title>
 
-      <Row gutter={[16, 16]}>
-        <Col span={24}>col</Col>
-        <Col span={24}>col</Col>
-        <Col span={24}>col</Col>
-        <Col span={24}>col</Col>
-        <Col span={24}>col</Col>
-      </Row>
+      <div className={styles["container"]}>
+        <Row gutter={[16, 16]}>
+          <Col span={7}>
+            <ArrowCard direction="left" label="Move shape" />
+          </Col>
+
+          <Col span={10}>
+            <Flex>
+              <ArrowCard direction="top-bottom" label="Move position" />
+            </Flex>
+          </Col>
+
+          <Col span={7}>
+            <ArrowCard direction="right" label="Move shape" />
+          </Col>
+        </Row>
+      </div>
     </Flex>
   );
 }
